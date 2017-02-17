8ea3f0cbb61d68a12fba9d67c78731411fe866f1
Facundo Di Pierro <facudipierro@gmail.com>
2017-02-17 15:59:29 -0300

    maquetado detalle de producto
    maquetado con estilos de detalle del producto

---
+++ detalle.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Listado - Test Frontend - Facundo Di Pierro - Mercadolibre</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		
+		<?php include('includes/search.php'); ?>
+		<div class="container">
+			<?php include('includes/breadcrumbs.php'); ?>
+			<div class="row listado-resultados">
+				<div class="col-md-12">
+					<div class="wrap-detalle">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-8">
+									<img src="productos/apple-ipod-touch-5g-16g-colores-big.jpg" width="680" height="580" alt="">
+								</div>
+								<div class="col-md-3">
+									<div class="detalle-condicion">Nuevo - 234 vendidos</div>
+									<div class="detalle-titulo">iPod Touch 8g Negro Impecable!</div>
+									<div class="detalle-precio">$1980<span>00</span></div>
+									<button class="detalle-bt-comprar">Comprar</button>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-8">
+									<div class="detalle-titulo-descripcion">
+										Descripción del Producto
+									</div>
+									<div class="detalle-descripcion">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id vulputate urna, et cursus urna. Aenean tempus porttitor nulla. Donec convallis velit a nisi euismod aliquet. Sed diam leo, facilisis at ipsum eu, ultricies tempus nunc. Cras bibendum, tellus sed porttitor ullamcorper, eros ex convallis dui, sit amet ullamcorper erat libero non massa. Donec tristique fringilla porta. Maecenas auctor diam a viverra molestie. 
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
